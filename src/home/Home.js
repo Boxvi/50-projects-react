ab4e4e4ae10644ac8744f71264dd71496a0bc717
Boris <hermosokawai@gmail.com>
2023-07-19 00:54:56 -0500

    DIA UNO COMPLETO con navegacion incluidad para el resto de los dias

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,20 @@
-
-
 const Home = () => {
     return (
-        <section className="section">
-            <h1>BORIS QUIZHPE <br/> PROYECTOS <br/> 50 dias 50 proyectos <br/> Contacto:  </h1>
+        <section className="sectiones">
+            <br/><span></span><br/><br/><br/>
+            <h1 style={{textAlign: "center"}}>Bienvenidos a mi Portafolio</h1>
+            <br/>
+            <h2 style={{textAlign: "center"}}>Boris Quizhpe Desarrollador de Software </h2>
+            <br/>
+            <h2 style={{textAlign: "center"}}><i className="bi bi-telephone"></i>Contacto: +593996474990</h2>
         </section>
     );
 };
 
 export default Home;
 
+/*
+
+            {/*<h2>BORIS QUIZHPE DESARROLLADOR DE SOFTWARE</h2
+/*<p >Centrame</p>
+ */
